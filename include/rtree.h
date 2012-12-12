9abbaf92259920409ba0656655f3619784c300fa
Sean Edwards <stedwards87@gmail.com>
2012-12-12 18:17:07 -0500

    Basic project structure for r-tree implementation.

---
+++ include/rtree.h
@@ -0,0 +1,4 @@
+#ifndef _R_TREE_H_
+#define _R_TREE_H_
+
+#endif // _R_TREE_H_
---
+++ test/test.h
@@ -0,0 +1,9 @@
+#ifndef _R_TREE_TEST_COMMON_H_
+#define _R_TREE_TEST_COMMON_H_
+
+#define BOOST_TEST_MODULE rtreetest
+#include <boost/test/unit_test.hpp>
+
+#include <rtree.h>
+
+#endif // _R_TREE_TEST_COMMON_H_
